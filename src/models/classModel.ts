6670f36336f2a1839f1c57fb0df1f8a14f42ae3d
Navindu Abhishek <abi2000navi@gmail.com>
2024-07-13 00:00:42 +0530

    create class model

---
+++ src/models/classModel.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const ClassSchema = new mongoose.Schema({
+    className: {
+        type: String,
+        required: [true, "Please provide the class name"]
+    },
+    roomCapacity: {
+        type: String,
+        required: [true, "Please provide room capacity"]
+    },
+    teacherId: {
+        type: String,
+    },
+    teacherName: {
+        type: String,
+
+    },
+    Subject: {
+        type: String,
+
+    },
+    teacherAvailability: {
+        type: String,
+
+    },
+})
+
+const Class = mongoose.models.classes || mongoose.model("classes", ClassSchema)
+
+export default Class
+
+
+
+
+
+
+
+

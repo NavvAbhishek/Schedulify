7ead71a2cfefdc9ddac6e352dc7c9cec4af36fa5
Navindu Abhishek <abi2000navi@gmail.com>
2024-08-17 19:31:33 +0530

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
+import { jwtVerify } from "jose";
 
-export function middleware(req: NextRequest) {
-
+export async function middleware(req: NextRequest) {
     const path = req.nextUrl.pathname
     const isPublicPath = path === "/login" || path === "/signup"
     const token = req.cookies.get('token')?.value || ""
@@ -13,12 +13,47 @@ export function middleware(req: NextRequest) {
     if (!isPublicPath && !token) {
         return NextResponse.redirect(new URL("/login", req.nextUrl))
     }
+
+    if (token) {
+        try {
+            const secret = new TextEncoder().encode(process.env.TOKEN_SECRET);
+
+            const { payload } = await jwtVerify(token, secret);
+            const userRole = payload.role;
+
+            const restrictedRoutesForTeachers = [
+                "/timetable-data",
+                "/timetable",
+            ];
+            if (userRole === "teacher" && restrictedRoutesForTeachers.includes(path)) {
+                return NextResponse.redirect(new URL("/", req.nextUrl));
+            }
+
+            const restrictedRoutesForStudents = [
+                "/timetable-data",
+                "/timetable",
+                "/teacher-dashboard",
+                "/admin-dashboard"
+            ];
+            if (userRole === "student" && restrictedRoutesForStudents.includes(path)) {
+                return NextResponse.redirect(new URL("/", req.nextUrl));
+            }
+
+        } catch (error) {
+            console.error("Invalid token", error);
+            return NextResponse.redirect(new URL("/login", req.nextUrl));
+        }
+    }
 }
 
 export const config = {
     matcher: [
-      '/login',
-      '/signup',
-      '/profile',
+        '/login',
+        '/signup',
+        '/profile',
+        '/timetable-data',
+        '/timetable',
+        "/teacher-dashboard",
+        "/admin-dashboard"
     ],
-  }
+}
